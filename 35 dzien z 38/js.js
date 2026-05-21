bc2bbd938d6a9e8716a0ce6d10c8b32dcb8fbb0e
Kamil6747 <kamlbaron3523@gmail.com>
2026-05-21 11:25:47 +0200

    PFD PHP

---
+++ 35 dzien z 38/js.js
@@ -0,0 +1,39 @@
+const ceny = {
+  rozmiar: {
+    "cienkie": 32,
+    "zwykle": 42,
+    "grube": 55
+  },
+  ciasto: {
+    "neapol": -6,
+    "rome": 0,
+    "nyc": 10
+  },
+  dodatki: {
+    "szynka": 5,
+    "salami": 4,
+    "prosciutto":3 ,
+    "boczek": 4,
+    "chorizo": 5,
+    "kurczak": 5,
+    "mortadela":4 ,
+    "gorgonzola":5 ,
+    "ser feta": 3,
+    "parmezan": 3,
+    "ser wędzony": 4,
+    "pieczarki": 2,
+    "rukola": 3,
+    "szuszone pomidory":4 ,
+    "czarne oliwki": 3,
+    "karczochy": 5,
+    "karmelizowana cebula":3 ,
+    "papryczki jalapeno": 6,
+    "ananas": 4,
+    "gruszka": 3,
+  }
+};
+
+let left = document.getElementById("left");
+for(let i = 0; i < ceny[rozmiar]; i++){
+console.log("A")
+}
